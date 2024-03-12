e310c9f39a91774c11cf16cd64407f3616fb1e64
Zain Ali <zaynmr810@gmail.com>
2024-03-12 06:00:30 +0500

    Javascript

---
+++ Javascript/test.js
@@ -0,0 +1,40 @@
+//var name = "Zain"
+//let lname = "Ali"
+//var age = 20
+//console.table([name,lname,age])
+//------------------------------------------------------------------------------
+//Conversion to Number
+// let num = "33"
+// let intNum = Number(num)
+// console.log(typeof intNum);
+// console.log(intNum);
+//------------------------------------------------------------------------------
+//Conversion to String
+// let num = 1
+// let stringNum = String(num)
+// console.log(typeof stringNum);
+// console.log(stringNum);
+//------------------------------------------------------------------------------
+//Conversion to Boolean
+// conversion of 1 to bool is true. "" to bool is false. "gg" to bool is true
+//------------------------------------------------------------------------------
+// let isLoggedIn = 1
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof boolIsLoggedIn);
+// console.log(boolIsLoggedIn);
+// let emptyString = ""
+// let boolEmptyString = Boolean(emptyString)
+// console.log(typeof boolEmptyString);
+// console.log(boolEmptyString);
+//------------------------------------------------------------------------------
+//Operations
+let val = 3
+let negVal = -val
+console.log(negVal);
+console.log(2**3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "3");
+console.log("1" + 2 + 3);
+console.log(true);
+console.log(+true);
