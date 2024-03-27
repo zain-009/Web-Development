d1bc1476160952f67e9818d540426c0711d96ffd
Zain Ali <zaynmr810@gmail.com>
2024-03-27 06:37:42 +0500

    javascript

--- Javascript/test.js
+++ Javascript/test.js
@@ -52,14 +52,13 @@
 //     console.log("I am a function");
 // }
 //------------------------------------------------------------------------------
-// let student = {                                           //this is an object
+// let student = { //this is an object
 //     name: "Zain",
 //     age: 20,
 //     cgpa: 3.4,
 // }
-//console.log(student.name);
-// for in returns keys of objects
-// for (let i in student) {
+// console.log(student.name);
+// for (let i in student) { // for in returns keys of objects
 //     console.log(i, student[i]);
 // }
 //console.log("Escape Char\t is one character");
@@ -174,13 +173,176 @@
 //         console.log(`Hello ${username}`);
 //     }
 // }
-function solve(n1,n2,...n3){
-    return n3;
-}
-solve(1,2,3,4,5,6); //values after 2 are arranged in an array due to rest operator
-//-----------------------Math--------------------------------------------------------
-//-----------------------Math--------------------------------------------------------
-//-----------------------Math--------------------------------------------------------
+// function solve(n1,n2,...n3){
+//     return n3;
+// }
+// solve(1,2,3,4,5,6); //values after 2 are arranged in an array due to rest operator
+// const user = {
+//     username: "Zain",
+//     age: 20,
+// }
+// function handleObject(anyobject) {
+//     console.log(`Username is ${anyobject.username}, Age: ${anyobject.age}`);
+// }
+// handleObject(user);
+// handleObject({
+//     username: "Ali",
+//     age: 20,
+// });
+//-----------------------Hoisting----------------------------------------------------
+// const add = function (num) {
+//     return num + 1;
+// }
+// add(5);
+// sub(2);
+// function sub(num) {
+//     return 5-num;
+// }
+//-----------------------Arrow-Functions---------------------------------------------
+// function hello() {
+//     let user = "Zain";
+//     const welcome = () => {
+//         console.log(user);
+//     }
+//     console.log(welcome());
+// }
+// hello();
+// const addOne = () => {
+//     return; //must add return statement
+// }
+// const addTwo = () => (
+//     1+2
+//     //no return statement
+//     );
+// const returnUser = () => ({name:"Zain"});
+//-----------------------IFFE--------------------------------------------------------
+// (function chai() {
+//     console.log("chai");
+// })();
+// ( (name) => {
+//     console.log(name);
+// })('Zain')
+//-----------------------Control-Flow------------------------------------------------
+// let userLoggedIn = true;
+// if(userLoggedIn) console.log("User is Logged in");
+// let temperature = 32;
+// switch (temperature) {
+//     case 32:
+//         console.log("It is hot today!");
+//         break;
+//     default:
+//         break;
+// }
+// const userEmail1 = [];
+// const userEmail2 = "";
+// if (userEmail1) {
+//     console.log("User has Email");
+// }
+// if (userEmail2) {
+//     console.log("User does not have Email");
+// }
+// -0, 0, null,"", undefined, NaN, BigInt 0n, are considered false
+// "0", 'false', " ", [], {}, function(){} is truthy because it is in quotes
+// const userEmail = [];
+// if (userEmail.length === 0) {
+//     console.log("Empty Array");
+// }
+// const empty = {}
+// if (Object.keys(empty).length === 0) {
+//     console.log("Object is Empty!");
+// }
+//-----------------------Nullish-Coalescing-Operator---------------------------------
+// let val1;
+// val1 = null ?? 10;
+//-----------------------Loops-------------------------------------------------------
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+// let val =0;
+// while (val < 10) {
+//     console.log(`val is ${val}`);
+//     val += 2;
+// }
+// const arr = [1,2,3,4,5];
+// for (const i of arr) {
+//     console.log(i);
+// }
+// const greetings = "Hello World";
+// for (const greet of greetings) {
+//     console.log("Each Char is " + greet);
+// }
+// const map = new Map(); //maps remeber order of key value pairs
+// map.set('Pk','Islamabad') //maps are not iteratable
+// map.set('US','Washigton')
+// map.set('Gr','Berlin')
+// map.set('Gr','Same key not entered in map')
+// for (const [key,value] of map) {
+//     console.log(key, '-',value);
+// }
+//for of does not work on objects
+// const lang = {
+//     cpp: "C++",
+//     rb: "Ruby"
+// }
+// for (const key in lang) {
+//     console.log(lang[key]);
+// }
+// const arrlang = ["C++","Ruby"];
+// for (const key in arrlang) {
+//     console.log(arrlang[key]);
+// }
+//array for of loop
+//objects for in loop
+// const coding = ["js","C++","Ruby"];
+// function print(item) {
+//     console.log(item);
+// }
+// coding.forEach( function (item) {console.log(item);} ) //callback function in array
+// coding.forEach( (item) => {console.log(item);} )
+// coding.forEach(print)
+// const myCoding = [{lang: "Javascript",name: "js"},{lang: "Java",name: "jv"}]
+// myCoding.forEach( (item) => {console.log(item.lang +" "+ item.name);} )
+//-----------------------Filter------------------------------------------------------
+// const nums = [1,2,3,4,5,6,7,8,9];
+// const newNums = nums.filter( (num) => num > 5 ); //returns values. For each does'nt return values
+// // if we open scope, then we need to say return num > 5
+// console.log(newNums);
+// const books = [
+//     {
+//         genre: "History",
+//         publish: 2003,
+//     },
+//     {
+//         genre: "Science",
+//         publish: 2003,
+//     },
+//     {
+//         genre: "Scifi",
+//         publish: 2003,
+//     }
+// ]
+// const myBooks = books.filter( (bk) => {
+//     return bk.genre === 'History' || bk.genre === 'Science'
+// } )
+// console.log(myBooks);
+
+//-----------------------Map-Operations----------------------------------------------
+// const myNum = [1,2,3,4,5];
+// // const newNums = myNum.map( (num) => num + 10 );
+// // console.log(newNums);
+// const chaining = myNum
+// .map( (num) => num * 10 ) //result is given to the chain below
+// .map( (num) => num + 1)
+// .filter( (num) => num > 30 );
+// console.log(chaining);
+
+const myNums = [1,2,3];
+const total = myNums.reduce( (acc,curr) => { 
+    console.log(`acc: ${acc} | curr: ${curr}`);
+    return acc + curr
+},0); // 0 is the initial accumulator value
+console.log(total);
+
 //-----------------------Math--------------------------------------------------------
 //-----------------------Math--------------------------------------------------------
 //-----------------------Math--------------------------------------------------------
