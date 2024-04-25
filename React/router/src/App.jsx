c04cb8a22c204d645d75fb7d13b29da25375de46
Zain Ali <zaynmr810@gmail.com>
2024-04-25 22:38:46 +0500

    React

--- React/router/src/App.jsx
+++ React/router/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
